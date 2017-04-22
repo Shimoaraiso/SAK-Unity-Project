05c44dc2b4d444103b11b99b744c601d55681af2
shimoaraiso <wataru.shimoaraiso@ssie.jp>
2017-04-22 10:57:31 +0900

    BackGroundControllerをリファクタリング

--- Assets/Scripts/BackGroundController.cs
+++ Assets/Scripts/BackGroundController.cs
@@ -1,37 +1,35 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 /// <summary>
 /// 【背景のコントロール用クラス】
-///		・背景は3枚、カメラから見切れたら回り込む
+///		背景は3枚、カメラから見切れたら回り込む
 /// </summary>
 public class BackGroundController : MonoBehaviour {
 	
 	// 背景の枚数
 	private int bgCount = 3;
 
-	private Transform myTfm;
-	private SpriteRenderer mySpriteR;
+	private Transform bgTfm;
+	private SpriteRenderer mySpriteRndr;
 	private float width;
 
-
 	void Start () {
-		myTfm = transform;
-		mySpriteR = GetComponent<SpriteRenderer>();
-		width = mySpriteR.bounds.size.x;
+		bgTfm = transform;
+		mySpriteRndr = GetComponent<SpriteRenderer>();
+		width = mySpriteRndr.bounds.size.x;
+		float bgWidth = mySpriteRndr.bounds.size.x;
 	}
 	
 	void Update () {
-		Vector3 myViewport = Camera.main.WorldToViewportPoint(myTfm.position);
-		//Debug.Log(viewport.x);
-
-		// 背景の回り込み
-		if (myViewport.x < -1.0f) {			
-			myTfm.position += Vector3.right * (width * bgCount);
+		Vector3 myViewport = Camera.main.WorldToViewportPoint(bgTfm.position);
+		
+		// 背景の回り込み(X軸プラス方向に移動時)
+		if (myViewport.x < -1.0f) {
+			bgTfm.position += Vector3.right * (width * bgCount);
 		}
+		// 背景の回り込み(X軸マイナス方向に移動時)
 		else if (myViewport.x > 2.1f) {
-			myTfm.position -= Vector3.right * (width * bgCount);
+			bgTfm.position -= Vector3.right * (width * bgCount);
 		}
 
 	}
